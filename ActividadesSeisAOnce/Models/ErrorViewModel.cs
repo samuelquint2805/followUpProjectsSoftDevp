f21e70458bebf26d29b2cfd7baf3952508970656
Samuel Quintero <quinterosamuel0528@gmail.com>
2025-11-24 20:32:22 -0500

    Actualizacion3, se ha quitado carpetas no funcionales

--- ActividadesSeisAOnce/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ActividadesSeisAOnce.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
